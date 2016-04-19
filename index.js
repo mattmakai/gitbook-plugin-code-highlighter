97f4882d8103244bf512a1651f1a70ed7ac7359a
Matt Makai <matthew.makai@gmail.com>
2016-04-19 10:58:25 +0200

    trying to get code block hook working

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ module.exports = {
     code: function(block) {
       // length is 3 if no custom mark is defined in plugins config
       var mark_len = highlight_mark.length;
+      console.log(block.body);
+      console.log("mark len: " + mark_len);
+
 
       // stores the line that may be highlighted
       var highlighted_block = '';
@@ -17,8 +20,8 @@ module.exports = {
         highlighted_block = "";
         strs = block.body.split("\n");
         for (var i = 0; i < strs.length; i++) {
-            if (strs[i].length > mark_len && strs[i].substring(0, mark_len) == "&&&") {
-                highlighted_block += '<span class="' + highlight_mark + '">' 
+            if (strs[i].length > mark_len && strs[i].substring(0, mark_len) == highlight_mark) {
+                highlighted_block += '<span class="' + highlight_css_class + '">' 
                 highlighted_block += strs[i].substring(mark_len) + "</span>" + "\n";
             } else {
                 highlighted_block += strs[i] + "\n";
