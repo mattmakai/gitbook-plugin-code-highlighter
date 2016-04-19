73fd8d7a48a7d9d1c5fb8e89d9c5ee06cf1b0067
Matt Makai <matthew.makai@gmail.com>
2016-04-19 10:22:43 +0200

    first version of plugin for testing

---
+++ index.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+var highlight_mark = "&&&";
+var highlight_css_class = "code-line-highlight";
+
+module.exports = {
+  book: assets,
+  ebook: assets,
+  blocks: {
+    code: function(block) {
+      // length is 3 if no custom mark is defined in plugins config
+      var mark_len = highlight_mark.length;
+
+      // stores the line that may be highlighted
+      var highlighted_block = '';
+
+      try {
+        // The process can fail (failed to parse)
+        highlighted_block = "";
+        strs = block.body.split("\n");
+        for (var i = 0; i < strs.length; i++) {
+            if (strs[i].length > mark_len && strs[i].substring(0, mark_len) == "&&&") {
+                highlighted_block += '<span class="' + highlight_mark + '">' 
+                highlighted_block += strs[i].substring(mark_len) + "</span>" + "\n";
+            } else {
+                highlighted_block += strs[i] + "\n";
+            }
+        }
+        //console.log(block);
+        //console.log(highlighted_block);
+      } catch(e) {
+        console.warn('Failed to highlight:');
+        console.warn(e);
+        highlighted_block = block.body;
+      }
+
+      return highlighted_block;
+    }
+  }
+};
